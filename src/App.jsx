8156edcc82bf94690f96846c93dd15d8b0605746
Derek Price <derekp098@gmail.com>
2021-03-07 00:57:48 -0800

    Now Gets solve time from timer and saves it in the timeArray state and sends that array to timestable to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
+import React, { useState } from 'react'
+import Timer from './components/Timer';
+import TimesTable from './components/TimesTable';
 
 function App() {
+
+  const [timesArray, setTimesArray] = useState([])
+
+  const handleTimesArray = (string) => {
+    var timesArrayCopy = timesArray.slice();
+    timesArrayCopy.push(string);
+    return timesArrayCopy;
+  }
   return (
     <div className="App">
-
+      <Timer updateTimesArray={(string) => setTimesArray(handleTimesArray(string))} />
+      <TimesTable timesArray={timesArray} />
     </div>
   );
 }
