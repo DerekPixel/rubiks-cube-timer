c20f98a68968c756338f95bcd34066a7cabc4efc
Derek Price <derekp098@gmail.com>
2021-03-07 23:39:10 -0800

    added function to delete time and added inspection switcher and state

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,50 @@ import Scrambler from './components/Scrambler';
 
 function App() {
 
-  const [timesArray, setTimesArray] = useState([])
+  const [timesArray, setTimesArray] = useState([]);
+  const [inspection, setInspection] = useState(true);
 
-  const handleTimesArray = (string) => {
+  const addNewTimeToTimesArray = (string) => {
     var timesArrayCopy = timesArray.slice();
     timesArrayCopy.push(string);
     return timesArrayCopy;
   }
+
+  const removeTimeFromTimeArray = (time) => {
+    var timesArrayCopy = timesArray.slice();
+    var indexToRemove = timesArrayCopy.indexOf(time);
+    timesArrayCopy.splice(indexToRemove, 1);
+    return timesArrayCopy;
+  }
+
+  const switchInspection = () => {
+    if(inspection) {
+      setInspection(false);
+    } else {
+      setInspection(true);
+    }
+  }
+
+
+
   return (
     <div className="App">
       <Scrambler />
-      <Timer updateTimesArray={(string) => setTimesArray(handleTimesArray(string))} />
-      <TimesTable timesArray={timesArray} />
+      <Timer 
+        updateTimesArray={(string) => setTimesArray(addNewTimeToTimesArray(string))} 
+        inspection={inspection}
+      />
+      <TimesTable 
+        timesArray={timesArray} 
+        removeTime={(time) => setTimesArray(removeTimeFromTimeArray(time))} 
+      />
+      <button
+        onClick={() => switchInspection()}
+      >
+        {
+          inspection ? 'Inspection is on' : 'Inspection is off'
+        }
+      </button>
     </div>
   );
 }
