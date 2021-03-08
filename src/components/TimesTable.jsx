b57b5de408451087fa3a341c70fa25fba8b70aac
Derek Price <derekp098@gmail.com>
2021-03-07 17:14:39 -0800

    added best and average time displays

--- src/components/TimesTable.jsx
+++ src/components/TimesTable.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
 
-const TimesTable = ({timesArray}) => {
+const TimesTable = ({timesArray = Array}) => {
+
+  const timeToString = (time) => {
+    var diffInHr = time / 3600000;
+    var hh = Math.floor(diffInHr).toString().padStart(2, "0");
+    var diffInMin = (diffInHr - hh) * 60;
+    var mm = Math.floor(diffInMin).toString().padStart(2, "0");
+    var diffInSec = (diffInMin - mm) * 60;
+    var ss = Math.floor(diffInSec).toString().padStart(2, "0");
+    var diffInMs = (diffInSec - ss) * 100;
+    var ms = Math.floor(diffInMs).toString().padStart(2, "0");
+    return `${hh}:${mm}:${ss}.${ms}`;
+  }
+
+  const timesArrayAverage = () => {
+    if(timesArray.length === 0) return;
+
+    var avg = 0;
+    for(var i = 0; i < timesArray.length; i++) {
+      avg+= timesArray[i];
+    }
+    return timeToString(avg/timesArray.length);
+  }
+
+  const bestTime = () => {
+    if(timesArray.length === 0) return;
+    return timeToString(Math.min(...timesArray));
+  }
 
   var times = timesArray.map((time, i) => 
-    
+
     <div
       key={i}
     >
-      {time}
+      #{i+1}: {timeToString(time)}
     </div>
 
   )
@@ -15,6 +41,8 @@ const TimesTable = ({timesArray}) => {
   return (
     <div>
       {times}
+      <div>avg: {timesArrayAverage()}</div>
+      <div>best: {bestTime()}</div>
     </div>
   )
 }
